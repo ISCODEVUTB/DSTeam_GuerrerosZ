636be5cdde53b86c67f8afd9e1fa7d7458aa5e0d
Alejandro Pedro Steinman Cuesta <steinman920@gmail.com>
2025-03-11 17:27:57 -0500

    fix test_Package.py 1.1

--- tests/test_Package.py
+++ tests/test_Package.py
@@ -13,8 +13,8 @@ class TestPackage(unittest.TestCase):
             observations="Fragile"
         )
 
-    @patch.object(PackageClassifier, 'classify', return_value="Standard")
-    @patch.object(PackageClassifier, 'calculate_cost', return_value=10.0)
+    @patch.object(PackageClassifier, 'classify', return_value="standard")
+    @patch.object(PackageClassifier, 'calculate_cost', return_value=17.0)
     def test_update_info(self, mock_calculate_cost, mock_classify):
         """Prueba que la actualización de información del paquete funcione correctamente."""
         self.package.update_info("20x20x20", 10.0, "Handle with care")
@@ -22,11 +22,11 @@ class TestPackage(unittest.TestCase):
         self.assertEqual(self.package.dimensions, "20x20x20")
         self.assertEqual(self.package.weight, 10.0)
         self.assertEqual(self.package.observations, "Handle with care")
-        self.assertEqual(self.package.type, "Standard")
-        self.assertEqual(self.package.shipping_cost, 10.0)
+        self.assertEqual(self.package.type, "standard")
+        self.assertEqual(self.package.shipping_cost, 17.0)
 
         mock_classify.assert_called_once_with(10.0)
-        mock_calculate_cost.assert_called_once_with(10.0, "Standard")  # Corregido
+        mock_calculate_cost.assert_called_once_with(10.0, "standard")  # Corregido
 
     def test_approve(self):
         """Prueba que `approve()` cambia correctamente el estado del paquete."""
