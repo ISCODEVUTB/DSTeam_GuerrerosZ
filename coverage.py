8abba7f719f01c060dcb272f4545c6da81eaf253
DevStein <83787781+SteinDevlop@users.noreply.github.com>
2025-03-12 14:03:20 -0500

    Update coverage.py

--- coverage.py
+++ coverage.py
@@ -6,8 +6,8 @@ skipsdist = True
 deps = [
     pytest,
     pytest-cov ]
-commands =
-    pytest --cov=src --cov-report=xml --cov-report=html
+commands = [
+    pytest --cov=src --cov-report=xml --cov-report=html ]
 
 [coverage:run]
 relative_files = True
