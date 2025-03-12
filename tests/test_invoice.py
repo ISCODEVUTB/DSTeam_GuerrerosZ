aca0cd96e75c1b1a871340a3ea4330bde824e3b7
DevStein <83787781+SteinDevlop@users.noreply.github.com>
2025-03-12 16:51:55 -0500

    hotfix src/ test_invoice.py

--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 # Importar después de modificar sys.path
 from src.invoice import Invoice
-from src.paymentmethod import PaymentMethod
+from src.payment_method import PaymentMethod
 
 class TestInvoice(unittest.TestCase):
     def setUp(self):
